2ecf33de9ac21846d7427a22b55eb2cbca71d162
Serhii Sapiannyk <serhii09@icloud.com>
2022-05-10 13:07:33 +0200

    fix code

--- src/components/pages/SingleTable.js
+++ src/components/pages/SingleTable.js
@@ -1,7 +1,6 @@
 import {  useSelector } from "react-redux"
 import { Navigate, useParams } from "react-router-dom"
 import { getTableById } from "../../Redux/tablesRedux"
-// import TableForm from "../features/TableForm/TableForm"
 import PropTypes from 'prop-types';
 import EditTableForm from "../features/EditTableForm";
 
@@ -13,7 +12,6 @@ const SingleTable = () => {
   if(!table) return <Navigate to="/"/>
   return (
     
-    // <TableForm table={table}/>
     <EditTableForm table={table}/>
   )
 }
